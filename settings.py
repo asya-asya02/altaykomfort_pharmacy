67d12a147e3c8e7095b58358397d0195b838d3c1
asya-asya02 <105218463+asya-asya02@users.noreply.github.com>
2022-05-15 13:42:24 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@
 HOST = 'localhost'
 PORT = '80'
 USER = 'user'
-PASSWORD = 'Neet2001' # 'Neet2001'
-DB_NAME = 'pharmacy' # 'pharmacy'
+PASSWORD = 'asya_asya_02'
+DB_NAME = 'pharmacy' 
 
 TOKEN = '5344722188:AAGGtyyEn-DO0kMZjUUWovIzBULRpS3C4BY'
 
@@ -12,5 +12,5 @@ TOKEN = '5344722188:AAGGtyyEn-DO0kMZjUUWovIzBULRpS3C4BY'
 IMG_PATH = '/var/www/pharmacy/data/img/'
 IMG_BAR = '/var/www/pharmacy/barcode/'
 
-"""IMG_PATH = '/home/neetq/projects/apteka/data/img/'
-IMG_BAR = '/home/neetq/projects/apteka/barcode/''"""
+"""IMG_PATH = '/home/asya_asya_02/projects/apteka/data/img/'
+IMG_BAR = '/home/asya_asya_02/projects/apteka/barcode/''"""
